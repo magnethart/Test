738c7a5aa02c8792c771d5a69ee560ba7ada40b8
magnethart <magnethart@gmail.com>
2013-12-22 17:15:19 -0500

    php test

---
+++ firstfile.php
@@ -0,0 +1,18 @@
+<?
+$new = 'testing github with php';
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?
+echo $new;
+?>
+
+</body>
+</html>
